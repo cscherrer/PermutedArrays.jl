0891a5d4346a653c1565879ee526d68c5b3133ef
Documenter.jl <documenter@juliadocs.github.io>
2021-06-19 12:26:40 +0000

    build based on 66faa63

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PermutedArrays","category":"page"},{"location":"#PermutedArrays","page":"Home","title":"PermutedArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PermutedArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PermutedArrays]","category":"page"}]
+}
