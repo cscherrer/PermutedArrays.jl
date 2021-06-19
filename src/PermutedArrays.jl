5597fb72fcb66e9cd94a555b98f9cc9e8ed75a64
Chad Scherrer <chad.scherrer@gmail.com>
2021-06-19 05:28:56 -0700

    Add the code

--- src/PermutedArrays.jl
+++ src/PermutedArrays.jl
@@ -1,5 +1,50 @@
 module PermutedArrays
 
-# Write your package code here.
+using Base.Order
 
+export PermutedVector
+struct PermutedVector{T, V} <: AbstractVector{T}
+    data::V
+    perm::Vector{Int}
+    iperm::Vector{Int}
 end
+
+function PermutedVector(v::V) where {T, V <: AbstractVector{T}}
+    n = length(v)
+    perm = collect(1:n)
+    iperm = copy(perm)
+    PermutedVector{T,V}(v, perm, iperm)
+end
+
+Base.size(v::PermutedVector) = size(v.data)
+Base.length(v::PermutedVector) = length(v.data)
+
+function Base.permute!(v::PermutedVector, p::Vector{Int})
+    permute!(v.perm, p)
+    invpermute!(v.iperm, p)
+    return v
+end
+
+Base.getindex(v::PermutedVector, i::Int) = getindex(v.data, v.perm[i])
+
+function Base.setindex!(v::PermutedVector, x, i::Int)
+    setindex!(v.data, x, v.perm[i])
+end
+
+function swap!(v::AbstractVector, i::Int, j::Int)
+    (v[i], v[j]) = (v[j], v[i])
+    return v
+end
+
+export swap!
+function swap!(v::PermutedVector, i::Int, j::Int)
+    p = v.perm
+    q = v.iperm
+    swap!(p, i, j)
+    swap!(q, p[i], p[j])
+    return v
+end
+
+swap!(::Nothing, i::Int, j::Int) = nothing
+
+end # module
